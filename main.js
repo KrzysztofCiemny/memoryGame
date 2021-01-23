7b1fc91c23c3b69daa894e4c1d50f182c8b4901f
KrzysztofCiemny <Ciemny.Krzysztof97@gmail.com>
2021-01-23 13:45:54 +0100

    added showing images on turning tiles

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let container = document.querySelector(".container"),
     tile = false,
-    tiles = [];
+    tiles = [],
+    tilesImg = ["url('img/kata.png')", "url('img/lucian.png')"];
 
 for (let i = 0; i < 4; i++) {
 
@@ -20,7 +21,8 @@ function turnTail(e) {
     let target = e.target,
         index = tiles.indexOf(target);
 
-    target.style.backgroundColor = "green";
+        target.style.backgroundColor = "white";
+        target.style.backgroundImage = tilesImg[index];
 
     tile = true;
 
